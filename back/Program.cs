5f2988a8861282590dd1c0a26dfe9688a0486f9a
FadyTawadrous <40478872+FadyTawadrous@users.noreply.github.com>
2025-08-15 17:41:02 +0300

    Fetching data using minimal api

--- back/Program.cs
+++ back/Program.cs
@@ -1,9 +1,34 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-services.AddDBContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseCors();
+
+// services.AddDBContext<AppDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/products",
+    () => new[] { new { Id = 1, Name = "Product 1", Price = 70.0m }, new { Id = 2, Name = "Product 2", Price = 50.0m } });
 
 app.Run();
