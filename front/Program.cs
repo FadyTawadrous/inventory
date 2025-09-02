913dd69f9ab23f7d81aa04b21ac7bfb613168bb4
FadyTawadrous <40478872+FadyTawadrous@users.noreply.github.com>
2025-09-02 18:16:18 +0300

    Adding local storage and session storage

--- front/Program.cs
+++ front/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+// using BlazorStateApp;
+using Blazored.LocalStorage;
+using Blazored.SessionStorage;
 
 using front;
 
@@ -13,5 +16,9 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddSingleton<UsersStateService>();
 builder.Services.AddSingleton<ProductsStateService>();
 builder.Services.AddHttpClient<TokenService>();
+
+// Adding Blazored LocalStorage and SessionStorage services to manage client-side storage.
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredSessionStorage();
 
 await builder.Build().RunAsync();
